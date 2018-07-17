504bf87cf4fc27cf488265a9c38aaba194b342d8
Cochise Ruhulessin <cochise.ruhulessin@wizardsofindustry.net>
2018-07-18 00:34:16 +0200

    qsa: Compile top-level controllers package.

---
+++ usr/app/ctrl/__init__.py
@@ -0,0 +1,4 @@
+"""Contains all controller implementations that are used by the WSGI
+appication. See also :class:`~usr.app.wsgi.application.WSGIApplication`.
+"""
+from .identification import IdentificationCtrl
