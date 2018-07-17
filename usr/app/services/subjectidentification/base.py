704f9eb0b2d2c1ddb7ac16003f1c2db7d3bae343
Cochise Ruhulessin <cochise.ruhulessin@wizardsofindustry.net>
2018-07-18 00:35:37 +0200

    qsa: Compile SubjectIdentificationService (app)

--- usr/app/services/subjectidentification/base.py
+++ usr/app/services/subjectidentification/base.py
@@ -1,12 +1,14 @@
 import ioc
 from sq.exceptions import ObjectDoesNotExist
 from sq.service import Service
+from sq.exceptions import UnprocessableEntity
 
 
 class BaseSubjectIdentificationService(Service):
     finder = ioc.class_property('SubjectFinder')
 
     SubjectDoesNotExist = type('SubjectDoesNotExist', (ObjectDoesNotExist,), {})
+    InvalidPrincipalType = type('InvalidPrincipalType', (UnprocessableEntity,), {})
 
     def identify(self):
         raise NotImplementedError("Subclasses must override this method.")
