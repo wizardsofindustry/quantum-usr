cfb6cae11e7ce3cf9a50d435bebc6082156b4988
Cochise Ruhulessin <cochise.ruhulessin@wizardsofindustry.net>
2018-07-18 21:20:54 +0200

    qsa: Update PrincipalRepository documentation

--- usr/infra/repo/principal/impl.py
+++ usr/infra/repo/principal/impl.py
@@ -4,13 +4,21 @@ from .base import BasePrincipalRepository
 class PrincipalRepository(BasePrincipalRepository):
 
     def persist(self, dto):
+        """Persists an association of a Principal to a Subject."""
         raise NotImplementedError("Subclasses must override this method.")
 
     def persist_email(self, gsid, email):
+        """Persists an association of an RFC822 email address to a Subject."""
         raise NotImplementedError("Subclasses must override this method.")
 
     def persist_x509_distinguished_name(self, gsid, distinguished_name):
+        """Persists an association of a Issuer DN/Subject DN combination to a
+        Subject.
+        """
         raise NotImplementedError("Subclasses must override this method.")
 
     def persist_x509_fingerprint(self, gsid, fingerprint):
+        """Persists an association of a X.509 certificate fingerprint to a
+        Subject.
+        """
         raise NotImplementedError("Subclasses must override this method.")
