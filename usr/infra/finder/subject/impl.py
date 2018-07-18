e28f35856284b7bfa1e79cf41b61cd0cea0b1a4c
Cochise Ruhulessin <cochise.ruhulessin@wizardsofindustry.net>
2018-07-18 08:26:15 +0200

    qsa: Update SubjectFinderdocumentation

--- usr/infra/finder/subject/impl.py
+++ usr/infra/finder/subject/impl.py
@@ -4,9 +4,13 @@ from .base import BaseSubjectFinder
 class SubjectFinder(BaseSubjectFinder):
 
     def by(self, principals=None):
+        """Resolve a list of Principal objects to a Global Subject Identifier
+        (GSID).
+        """
         raise NotImplementedError("Subclasses must override this method.")
 
     def by_email(self, email):
+        """Resolve a Subject by email address."""
         raise NotImplementedError("Subclasses must override this method.")
 
     #def by_certificate_fingerprint(self):
