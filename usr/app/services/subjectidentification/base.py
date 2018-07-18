e271fdc2db44c751a9191c081971aaecfe205e93
Cochise Ruhulessin <cochise.ruhulessin@wizardsofindustry.net>
2018-07-18 06:57:52 +0200

    qsa: Compile SubjectIdentificationService (app)

--- usr/app/services/subjectidentification/base.py
+++ usr/app/services/subjectidentification/base.py
@@ -10,10 +10,10 @@ class BaseSubjectIdentificationService(Service):
     SubjectDoesNotExist = type('SubjectDoesNotExist', (ObjectDoesNotExist,), {})
     InvalidPrincipalType = type('InvalidPrincipalType', (UnprocessableEntity,), {})
 
-    def identify(self):
+    def identify(self, principal):
         raise NotImplementedError("Subclasses must override this method.")
 
-    def identify_x509(self):
+    def identify_x509(self, crt):
         raise NotImplementedError("Subclasses must override this method.")
 
 
