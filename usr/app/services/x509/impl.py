2c9e4f1f84f742a10978350f72f8e3496e7c92e0
Cochise Ruhulessin <cochise.ruhulessin@wizardsofindustry.net>
2018-07-18 07:31:58 +0200

    qsa: Update X509Service (app) documentation

--- usr/app/services/x509/impl.py
+++ usr/app/services/x509/impl.py
@@ -4,13 +4,19 @@ from .base import BaseX509Service
 class X509Service(BaseX509Service):
 
     def principals_from_pem(self):
+        """Extract Principals from a X.509 client certificate."""
         raise NotImplementedError("Subclasses must override this method.")
 
     def email_from_san(self):
+        """Extract email addresses from the Subject Alternative Names (SANs)."""
         raise NotImplementedError("Subclasses must override this method.")
 
     def fingerprint(self):
+        """Extract the fingerprint from a X.509 certificate."""
         raise NotImplementedError("Subclasses must override this method.")
 
     def distinguished_names(self):
+        """Return a tuple containing the Issuer Distinguished Name (DN) and the
+        Subject DN.
+        """
         raise NotImplementedError("Subclasses must override this method.")
