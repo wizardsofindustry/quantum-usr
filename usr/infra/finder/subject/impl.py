b533b8529bc1953aae982777bf4946b48646d49d
Cochise Ruhulessin <cochise.ruhulessin@wizardsofindustry.net>
2018-07-18 08:26:08 +0200

    Further specify SubjectFinder

--- usr/infra/finder/subject/impl.py
+++ usr/infra/finder/subject/impl.py
@@ -3,17 +3,17 @@ from .base import BaseSubjectFinder
 
 class SubjectFinder(BaseSubjectFinder):
 
-    def by(self):
+    def by(self, principals=None):
         raise NotImplementedError("Subclasses must override this method.")
 
-    def by_email(self):
+    def by_email(self, email):
         raise NotImplementedError("Subclasses must override this method.")
 
-    def by_certificate_fingerprint(self):
-        raise NotImplementedError("Subclasses must override this method.")
+    #def by_certificate_fingerprint(self):
+    #    raise NotImplementedError("Subclasses must override this method.")
 
-    def by_distinguished_name(self):
-        raise NotImplementedError("Subclasses must override this method.")
+    #def by_distinguished_name(self):
+    #    raise NotImplementedError("Subclasses must override this method.")
 
-    def by_keyid(self):
-        raise NotImplementedError("Subclasses must override this method.")
+    #def by_keyid(self):
+    #    raise NotImplementedError("Subclasses must override this method.")
