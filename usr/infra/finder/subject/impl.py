64799bc4c644800e7e937d261ca68a4af7540e89
Cochise Ruhulessin <cochise.ruhulessin@wizardsofindustry.net>
2018-07-18 22:30:43 +0200

    qsa: Update SubjectFinderdocumentation

--- usr/infra/finder/subject/impl.py
+++ usr/infra/finder/subject/impl.py
@@ -40,10 +40,20 @@ class SubjectFinder(BaseSubjectFinder):
         })
 
     def by_x509_fingerprint(self):
+        """Resolve a Subject by using the fingerprint of a X.509 certificate,
+        issued by a trused Certification Authority (CA).
+        """
         raise NotImplementedError("Subclasses must override this method.")
 
     def by_x509_distinguished_names(self):
+        """Resolve a Subject using the Distinguished Names (DNs) of the Issuer
+        and Subject on a X.509 certificate, issued by a trusted Certification
+        Authority (CA).
+        """
         raise NotImplementedError("Subclasses must override this method.")
 
     def by_x509_keyid(self):
+        """Resolve a Subject using the Subject Key Identifier (SKI) on a X.509
+        certificate, issued by a trusted Certification Authority (CA).
+        """
         raise NotImplementedError("Subclasses must override this method.")
