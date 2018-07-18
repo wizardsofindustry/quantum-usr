9372274eaae2c0f22ecee1b8221eef3238f74058
Cochise Ruhulessin <cochise.ruhulessin@wizardsofindustry.net>
2018-07-18 06:57:53 +0200

    qsa: Update SubjectIdentificationService (app) documentation

--- usr/app/services/subjectidentification/impl.py
+++ usr/app/services/subjectidentification/impl.py
@@ -4,7 +4,11 @@ from .base import BaseSubjectIdentificationService
 class SubjectIdentificationService(BaseSubjectIdentificationService):
 
     def identify(self):
+        """Identify a Subject using the given Principal object."""
         raise NotImplementedError("Subclasses must override this method.")
 
     def identify_x509(self):
+        """Identify a Subject using the Issuer Distinguished Name (DN) and
+        Subject DN, email address and certificate fingerprint.
+        """
         raise NotImplementedError("Subclasses must override this method.")
