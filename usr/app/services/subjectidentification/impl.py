34c39abee88ad6ce39f750872c184e2d92f2632a
Cochise Ruhulessin <cochise.ruhulessin@wizardsofindustry.net>
2018-07-18 07:08:59 +0200

    Implement SubjectIdentificationService.identify()

--- usr/app/services/subjectidentification/impl.py
+++ usr/app/services/subjectidentification/impl.py
@@ -3,12 +3,26 @@ from .base import BaseSubjectIdentificationService
 
 class SubjectIdentificationService(BaseSubjectIdentificationService):
 
-    def identify(self):
+    def identify(self, principal):
         """Identify a Subject using the given Principal object."""
-        raise NotImplementedError("Subclasses must override this method.")
+        principal_type = principal.pop('type', None)
+        if principal_type is None:
+            raise TypeError('Missing `type` attribute.')
+        if principal_type not in self.allowed_methods:
+            raise self.UnknownPrincipalType(
+                f'Unknown Principal Type: {principal_type}')
+        func = getattr(self, f'identify_{principal_type}', None)
+        if func is None:
+            raise self.UnknownPrincipalType(
+                f'Unknown Principal Type: {principal_type}')
+        return func(**principal)
 
-    def identify_x509(self):
+    def identify_x509(self, crt):
         """Identify a Subject using the Issuer Distinguished Name (DN) and
         Subject DN, email address and certificate fingerprint.
         """
         raise NotImplementedError("Subclasses must override this method.")
+
+    # TODO: Docstring updating messes up the code if this is put at the
+    # top of the class.
+    allowed_methods = ['x509']
