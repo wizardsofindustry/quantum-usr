541b5473f88adc65e9c9eb8bffabeccabb56e283
Cochise Ruhulessin <cochise.ruhulessin@wizardsofindustry.net>
2018-07-18 21:37:42 +0200

    Add migration scripts

---
+++ migrations/versions/f406c848e440_create_subjects_relation.py
@@ -0,0 +1,25 @@
+"""Create subjects relation
+
+Revision ID: f406c848e440
+Revises: b0a1f8d10af2
+Create Date: 2018-07-18 12:11:49.123223
+
+"""
+from alembic import op
+import sqlalchemy as sa
+import sq.ext.rdbms.types
+
+
+# revision identifiers, used by Alembic.
+revision = 'f406c848e440'
+down_revision = 'b0a1f8d10af2'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
