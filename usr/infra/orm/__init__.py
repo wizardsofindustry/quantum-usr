c47d7e2f899642a3b926a6ed75b447460bda274d
Cochise Ruhulessin <cochise.ruhulessin@wizardsofindustry.net>
2018-07-18 22:40:59 +0200

    qsa: Compile top-level ORM package

--- usr/infra/orm/__init__.py
+++ usr/infra/orm/__init__.py
@@ -1,4 +1,5 @@
 from .certificatefingerprints import CertificateFingerprint
+from .certificatekeyids import CertificateKeyIdentifier
 from .certificatenames import CertificateNames
 from .emailaddresses import EmailAddress
 from .base import Relation
