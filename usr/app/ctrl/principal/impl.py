05a0a6cce937499e4428764fa0d756ea320148a2
Cochise Ruhulessin <cochise.ruhulessin@wizardsofindustry.net>
2018-07-18 20:30:43 +0200

    Implement PrincipalController.post()

--- usr/app/ctrl/principal/impl.py
+++ usr/app/ctrl/principal/impl.py
@@ -3,6 +3,16 @@ from .base import BasePrincipalController
 
 
 class PrincipalController(BasePrincipalController):
+    """Provides a handler function for ``POST`` requests holding a
+    **SubjectPrincipalSet** and associates them to the appropriate
+    **Subject**.
+    """
 
     async def post(self, request, *args, **kwargs):
-        raise NotImplementedError("Subclasses must override this method.")
+        """Deserialize a **SubjectPrincipalSet** from the request payload
+        and associate the enclosed **Principals** to the specified
+        **Subject**.
+        """
+        for principal in request.payload.principals:
+            self.subject.associate(request.payload.gsid, principal)
+        return self.render_to_response({}, status_code=200)
