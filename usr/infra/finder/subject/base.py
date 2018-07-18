d8dd1e5e012a239b02794968c64b9643bab3d3d6
Cochise Ruhulessin <cochise.ruhulessin@wizardsofindustry.net>
2018-07-18 08:26:15 +0200

    qsa: Compile SubjectFinder

--- usr/infra/finder/subject/base.py
+++ usr/infra/finder/subject/base.py
@@ -5,19 +5,10 @@ from sq.readmodel import Finder
 class BaseSubjectFinder(Finder):
     session = ioc.class_property('DatabaseSessionFactory')
 
-    def by(self):
+    def by(self, principals):
         raise NotImplementedError("Subclasses must override this method.")
 
-    def by_email(self):
-        raise NotImplementedError("Subclasses must override this method.")
-
-    def by_certificate_fingerprint(self):
-        raise NotImplementedError("Subclasses must override this method.")
-
-    def by_distinguished_name(self):
-        raise NotImplementedError("Subclasses must override this method.")
-
-    def by_keyid(self):
+    def by_email(self, email):
         raise NotImplementedError("Subclasses must override this method.")
 
 
