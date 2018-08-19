ede78b5b0227dfe134ec8497942f0dc015656717
Cochise Ruhulessin <cochise.ruhulessin@wizardsofindustry.net>
2018-08-19 11:54:05 +0200

    Spec phonenumber handling

--- usr/app/wsgi/tests/test_principal.py
+++ usr/app/wsgi/tests/test_principal.py
@@ -10,6 +10,7 @@ from ..endpoints import PrincipalEndpoint
 
 class CreatePrincipalTestCase(sq.test.SystemTestCase):
     metadata = orm.Relation.metadata
+    gsid = "00000000-0000-0000-0000-000000000000"
 
     def setUp(self):
         super(CreatePrincipalTestCase, self).setUp()
@@ -19,19 +20,36 @@ class CreatePrincipalTestCase(sq.test.SystemTestCase):
             self.crt = sq.lib.x509.Certificate.frompem(self.pem)
 
     @sq.test.integration
+    def test_associate_phonenumber_responds_ok(self):
+        """Associate a X.509 certificate to a **Principal**."""
+
+        request = sq.test.request_factory(
+            method='POST',
+            json={
+                'gsid': self.gsid,
+                'principals': [
+                    {'type': 'phonenumber', 'phonenumber': "+31612345678"}
+                ]
+            }
+        )
+        response = self.run_callable(self.loop, self.endpoint.handle, request)
+        self.assertEqual(response.status_code, 200)
+
+    @sq.test.integration
     def test_associate_x509_responds_ok(self):
         """Associate a X.509 certificate to a **Principal**."""
 
         request = sq.test.request_factory(
             method='POST',
             json={
-                'gsid': "00000000-0000-0000-0000-000000000000",
+                'gsid': self.gsid,
                 'principals': [
                     {'type': 'x509', 'crt': bytes.hex(self.pem)}
                 ]
             }
         )
-        self.run_callable(self.loop, self.endpoint.handle, request)
+        response = self.run_callable(self.loop, self.endpoint.handle, request)
+        self.assertEqual(response.status_code, 200)
 
 
 class MockRequest:
