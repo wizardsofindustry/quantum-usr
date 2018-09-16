79b7fa5d41ebf5a9de94d941857231e875105742
Cochise Ruhulessin <cochise.ruhulessin@wizardsofindustry.net>
2018-09-16 13:22:12 +0200

    Implement Bank Identification Number (BIN, from iDIN)

--- usr/app/wsgi/tests/test_identification_idin_bin.py
+++ usr/app/wsgi/tests/test_identification_idin_bin.py
@@ -13,7 +13,7 @@ from ..endpoints import IdentificationEndpoint
 @sq.test.integration
 class PhonenumberSubjectIdentificationTestCase(sq.test.SystemTestCase):
     gsid = "00000000-0000-0000-0000-000000000000"
-    phonenumber = "+31612345678"
+    bin = "CMBANL9Z3xOcyYKUhR8s0mS+tbkNO2xF2/U/Ns3eIyMOWYWmOZeUGw8StPKPhAdRTyN1XWne1rgJQA"
     metadata = orm.Relation.metadata
 
     def setUp(self):
@@ -21,13 +21,13 @@ class PhonenumberSubjectIdentificationTestCase(sq.test.SystemTestCase):
         self.endpoint = IdentificationEndpoint()
         self.service = ioc.require('SubjectIdentificationService')
         self.service.associate(self.gsid,
-            {'type': 'phonenumber', 'phonenumber': self.phonenumber})
+            {'type': 'idin:bin', 'bin': self.bin})
 
-    def test_subject_is_identified_by_phonenumber(self):
-        """Identify a Subject by phonenumber."""
+    def test_subject_is_identified_by_bin(self):
+        """Identify a Subject by Bank Identification Number (BIN)."""
         dto = {
-            'type': 'phonenumber',
-            'phonenumber': self.phonenumber
+            'type': 'idin:bin',
+            'bin': self.bin
         }
         response = self.request(self.endpoint.handle, method='POST', json=dto)
         self.assertEqual(response.status_code, 200)
@@ -36,11 +36,11 @@ class PhonenumberSubjectIdentificationTestCase(sq.test.SystemTestCase):
         self.assertIn('gsid', result)
         self.assertEqual(result['gsid'], self.gsid.replace('-', ''))
 
-    def test_subject_is_not_identified_by_unknown_phonenumber(self):
-        """Identify a Subject by phonenumber that does not exist."""
+    def test_subject_is_not_identified_by_unknown_bin(self):
+        """Identify a Subject by BIN that does not exist."""
         dto = {
-            'type': 'phonenumber',
-            'phonenumber': "+31687654321"
+            'type': 'idin:bin',
+            'bin': "bla"
         }
         response = self.request(self.endpoint.handle, method='POST', json=dto)
         self.assertEqual(response.status_code, 404)
